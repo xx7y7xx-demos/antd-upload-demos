f14ecd937539309642543f094b66212f4dc08ce3
devin.chenyang <d3vin.chen@gmail.com>
2018-06-07 00:23:03 +0800

    Add upload component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Upload from 'antd/lib/upload'
+import Icon from 'antd/lib/icon'
+
+import logo from './logo.svg'
+import './App.css'
+
+const Dragger = Upload.Dragger
+const log = console.log
 
 class App extends Component {
   render() {
+    const props = {
+      name: 'file',
+      multiple: true,
+      action: '/poi/bulkuploads',
+      headers: {
+        Authorization: localStorage.getItem('jwt')
+      },
+      onChange: this.handleChange
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +28,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Dragger {...props}>
+          <p className="ant-upload-drag-icon">
+            <Icon type="inbox" />
+          </p>
+          <p className="ant-upload-text">
+            Click or drag file to this area to upload
+          </p>
+          <p className="ant-upload-hint">
+            Support for a single or bulk upload.
+          </p>
+        </Dragger>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
